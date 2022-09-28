c87087e2a44052657d8ac9f90e3eb795edfeb855
edufrasson <edudomine2016@gmail.com>
2022-09-28 15:19:14 -0300

    Adicionar arquivos de projeto.

---
+++ AppGuiaCursoDS/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppGuiaCursoDS
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Inicial)));
+        }
+
+        private void Open_Inicial(object sender, EventArgs e)
+        {
+            Console.WriteLine("a");
+        }
+
+        private void Open_Primeiro(object sender, EventArgs e)
+        {
+            Console.WriteLine("a");
+        }
+
+        private void Open_Segundo(object sender, EventArgs e)
+        {
+            Console.WriteLine("a");
+        }
+
+        private void Open_Terceiro(object sender, EventArgs e)
+        {
+            Console.WriteLine("a");
+        }
+
+        private void Open_Vestibulinho(object sender, EventArgs e)
+        {
+            Console.WriteLine("a");
+        }
+        private void Open_Contato(object sender, EventArgs e)
+        {
+            Console.WriteLine("a");
+        }
+    }
+}
---
+++ AppGuiaCursoDS/Model/Componente.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppGuiaCursoDS.Model
+{
+    public class Componente
+    {
+    }
+}
